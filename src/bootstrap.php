4aa430d2213d418b42ee9886071d95529047e162
Richard Hinkamp <richard@hinkamp.nl>
2014-03-01 17:32:56 +0100

    Create base bootstrap file

---
+++ src/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * This file is part of the Bolt skeleton package.
+ *
+ * (c) Richard Hinkamp <richard@hinkamp.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once( __DIR__ . '/../vendor/bolt/bolt/app/bootstrap.php' );
